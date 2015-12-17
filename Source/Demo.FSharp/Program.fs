8810fd95da199e42a0878ec5d68530b1bd2358f9
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-12-17 19:27:12 +0200

    FSharp API

---
+++ Source/Demo.FSharp/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open System.Reflection
+
+open Orleankka
+open Orleankka.FSharp
+open Orleankka.Playground
+
+type GreeterMessage = 
+   | Greet of string
+   | Hi
+
+type Greeter() = 
+   inherit Actor<GreeterMessage>()   
+
+   override this.Receive message reply = task {
+      match message with
+      | Greet who -> printfn "Hello %s" who
+      | Hi -> printfn "Hello from F#!"           
+   }
+
+[<EntryPoint>]
+let main argv = 
+
+    printfn "Running demo. Booting cluster might take some time ...\n"
+
+    use system = ActorSystem.Configure()
+                            .Playground()
+                            .Register(Assembly.GetExecutingAssembly())
+                            .Done()
+                  
+    let actor = system.ActorOf<Greeter>(Guid.NewGuid().ToString())
+
+    let task() = task {
+      do! actor <! Hi 
+      do! actor <! Greet "Yevhen"
+      do! actor <! Greet "AntyaDev"
+    }
+    
+    Task.run(task) |> ignore
+    
+    Console.ReadLine() |> ignore    
+    0
