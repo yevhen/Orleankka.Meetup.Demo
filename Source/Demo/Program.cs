06163a9daccf3a250dc570ec28de57d563dc94df
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-12-17 17:14:13 +0200

    Hello, world

--- Source/Demo/Program.cs
+++ Source/Demo/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace Demo
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Orleankka;
+using Orleankka.Playground;
+
+namespace Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var system = ActorSystem.Configure()
+                .Playground()
+                .Register(Assembly.GetExecutingAssembly())
+                .Done();
+
+            Run(system).Wait();
+            Console.ReadKey(true);
+
+            system.Dispose();
+        }
+
+        static async Task Run(IActorSystem system)
+        {
+            var foo = system.ActorOf<Foo>("world");
+            Console.WriteLine(await foo.Ask<string>("Hello"));
+        }
+
+        class Foo : Actor
+        {
+            string On(string msg) => $"{msg}, {Id}!";
         }
     }
 }
