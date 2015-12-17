cd6a2b1737b01ee584312657d8fc4d48fd349f95
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2015-12-17 20:14:12 +0200

    Revert/prepare for a Streams demo

--- Source/Demo/Program.cs
+++ Source/Demo/Program.cs
@@ -25,50 +25,18 @@ namespace Demo
 
         static async Task Run(IActorSystem system)
         {
-            var foo = system.ActorOf<Foo>("#");
-            WriteLine(await foo.Ask<string>("test"));
+            var foo = system.ActorOf<Foo>("world");
+            WriteLine(await foo.Ask<string>(new Bar {Text = "Hello"}));
         }
-    }
-
-    public class Foo : Actor
-    {
-        public Foo()
-        {}
-
-        public Foo(string id, IActorRuntime runtime)
-            : base(id, runtime)
-        {}
-
-        Task<string> On(string msg)
+        
+        [Serializable] class Bar
         {
-            var bar = System.ActorOf<Bar>("#");
-
-            var message = new Request
-            {
-                Text = msg,
-                Date = DateTime.UtcNow
-            };
-
-            try
-            {
-                return bar.Ask<string>(message);
-            }
-            catch (ApplicationException)
-            {
-                return Task.FromResult("default");
-            }
+            public string Text;
         }
-    }
 
-    [Serializable]
-    public class Request
-    {
-        public string Text;
-        public DateTime Date;
-    }
-
-    public class Bar : Actor
-    {
-        string On(Request msg) => $"{Self}: {msg.Text} - {msg.Date}, !";
+        class Foo : Actor
+        {
+            string On(Bar msg) => $"{msg.Text}, {Id}!";
+        }
     }
 }
